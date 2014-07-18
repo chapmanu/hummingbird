33a881cb68a126094acd44e7be2704841a79b630
Miles Zimmerman <miles@zserver.org>
2014-07-17 21:46:28 -0700

    Added initializer

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
-var fs     = require('fs');
-var events = require('events');
-
-/**
- * Initialize the hummingbird namespace
- */
-var H = {
+(function() {
+  var hummingbird;
+  try {
+    hummingbird = require('./lib');
+  } catch (e) {
+    console.log('An error occured while starting hummingbird.');
+    console.log(e);
+    return;
+  }
   
-  // main config
-  config: require('./config.js'),
-  
-  // service config
-  services: {},
-  
-  // plugins
-  plugins: []
-  
-};
-
-
-// Load services
-fs.readdirSync('./services').forEach(function(s) {
-  H.services[s.replace('.js','')] = require('./services/'+s);
-});
-
-
-// Load plugins
-H.config.plugins.forEach(function(plugin) {
-  H.plugins.push(require('./plugins/'+plugin+'.js'));
-});
-
-
-
-// Make it global
-global.hummingbird = H;
-
-
-// Set Process ENV
-hummingbird.config.port        = process.env['PORT']     || hummingbird.config.port;
-hummingbird.config.environment = process.env['NODE_ENV'] || hummingbird.config.environment;
-
-
-// Start the core
-require('hummingbird-core');
+  console.log('Initializing Hummingbird...');
+  hummingbird.initialize();
+})();
